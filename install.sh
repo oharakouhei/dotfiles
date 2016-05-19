4039e7513858fd66d9a2fdaf282ffee127af66b3
KoheiOhara <gofue0723@gmail.com>
2016-05-19 17:32:31 +0900

    [add] gitconfig

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ ln -fs ~/dotfiles/_vimrc ~/.vimrc
 ln -fs ~/dotfiles/_gvimrc ~/.gvimrc
 ln -fs ~/dotfiles/_zshrc ~/.zshrc
 ln -fs ~/dotfiles/_tmux.conf ~/.tmux.conf
-
+ln -fs ~/dotfiles/_gitconfig ~/.gitconfig
 
 #"""""""""""" NeoBundle """""""""""""
 ln -fs ~/dotfiles/vimfiles ~/.vim
-mkdir ~/dotfiles/vimfiles/bundle
+mkdir -p ~/dotfiles/vimfiles/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/vimfiles/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc ~/dotfiles/vimfiles/bundle/vimproca
